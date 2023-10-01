0d167de7c56632c44cd01a4169ad48532b8a85ef
Sandra Animon <75678695+SANDRAANIMON@users.noreply.github.com>
2023-10-01 23:57:44 +0530

    Create Grid Unique Paths.cpp

---
+++ Day - 03/Grid Unique Paths.cpp
@@ -0,0 +1,26 @@
+/*CodeStudio solution*/
+#include <bits/stdc++.h> 
+int uniquePaths(int m, int n) {
+	// Write your code here.
+    int N = m+n-2;
+    int r = m-1;
+    double res = 1;
+        
+    for(int i=1; i<=r; i++)
+        res = res*(N-r+i)/i;
+    return (int)res;
+}
+
+/*Leetcode solution*/
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int N = m+n-2;
+        int r = m-1;
+        double res = 1;
+        
+        for(int i=1; i<=r; i++)
+            res = res*(N-r+i)/i;
+        return (int)res;
+    }
+};
