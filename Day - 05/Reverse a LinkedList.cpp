ea41d485e941ea6817093703e369f41810dc71d5
Sandra Animon <75678695+SANDRAANIMON@users.noreply.github.com>
2023-11-12 00:01:27 +0530

    Create Reverse a LinkedList.cpp

---
+++ Day - 05/Reverse a LinkedList.cpp
@@ -0,0 +1,62 @@
+/*CodeStudio solution*/
+
+#include <bits/stdc++.h> 
+/****************************************************************
+
+    Following is the class structure of the LinkedListNode class:
+
+    template <typename T>
+    class LinkedListNode
+    {
+    public:
+        T data;
+        LinkedListNode<T> *next;
+        LinkedListNode(T data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+*****************************************************************/
+
+LinkedListNode<int> *reverseLinkedList(LinkedListNode<int> *head) 
+{
+    // Write your code here
+    LinkedListNode<int> *dummynode = NULL;
+    while(head != NULL){
+        LinkedListNode<int> *nextt = head->next;
+        head->next = dummynode;
+        dummynode = head;
+        head = nextt;
+    }
+    return dummynode;
+}
+
+
+/*Leetcode solution*/
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode *dummynode = NULL;
+        while(head != NULL){
+            ListNode *nextt = head->next;
+            head->next = dummynode;
+            dummynode = head;
+            head = nextt;
+        }
+        return dummynode;
+    }
+};
