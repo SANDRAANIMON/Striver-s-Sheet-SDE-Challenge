a4f35b8dec8cba6c54f7745c3e3e352db46f6b29
Sandra Animon <75678695+SANDRAANIMON@users.noreply.github.com>
2023-11-11 23:49:09 +0530

    Update 2-Sum Problem.cpp

--- Day - 04/2-Sum Problem.cpp
+++ Day - 04/2-Sum Problem.cpp
@@ -22,4 +22,26 @@ vector<vector<int>> pairSum(vector<int> &arr, int s){
     }
     sort(r.begin(), r.end());
     return r;
-}
+}class Solution
+{
+  public:
+    vector<int> twoSum(vector<int>& nums, int target)
+    {
+        unordered_map<int, int> seen;
+        for(int i=0; i<nums.size(); i++)
+        {
+            int firstNumber = nums[i], secondNumber = target-firstNumber;  // firstNumber + secondNumber = target
+            
+            if(seen.find(secondNumber) != seen.end())
+                return {seen[secondNumber], i};
+            else
+                seen[firstNumber] = i;
+        }
+        return {};
+    }
+};
+
+
+/*Leetcode Solution*/
+
+
