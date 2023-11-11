41267e7380facf3fdcf31ce33fdf08b0cc3bdb0e
Sandra Animon <75678695+SANDRAANIMON@users.noreply.github.com>
2023-11-11 23:54:23 +0530

    Update Longest Consecutive Sequence.cpp

--- Day - 04/Longest Consecutive Sequence.cpp
+++ Day - 04/Longest Consecutive Sequence.cpp
@@ -22,3 +22,27 @@ int lengthOfLongestConsecutiveSequence(vector<int> &arr, int n) {
     return ans;
 }
 
+/*Leetcode Solution*/
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> num;
+        int ans = 0;
+        
+        for(int i : nums) {
+            num.insert(i);
+        }
+        
+        for(int i : nums) {
+            if(!num.count(i-1)) {
+                int count = 1;
+                while(num.count(i+count)) {
+                    count++;
+                }
+                ans = max(ans, count);
+            }
+        }
+        return ans;
+    }
+};
